1f78fe21a667dca36445a87657d757dd47556d37
OsipXD <osip.fatkullin@gmail.com>
2018-09-03 00:56:23 +0300

    Add tests for IndexedMap and `roundToPowerOf`

--- core/bukkit/src/main/java/ru/endlesscode/rpginventory/extensions/MathExtensions.kt
+++ core/bukkit/src/main/java/ru/endlesscode/rpginventory/extensions/MathExtensions.kt
@@ -1,8 +1,11 @@
 package ru.endlesscode.rpginventory.extensions
 
+import kotlin.math.sign
+
 /**
  * Rounds the number up to given [power].
  */
 fun Int.roundToPowerOf(power: Int): Int {
-    return this / power * power
+    val additional = if (this % power == 0) 0 else this.sign
+    return (this / power + additional) * power
 }
