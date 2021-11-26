976b5345f0c350a5f5f686e2a61fcae41cfdee5f
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-11-27 00:03:38 +0300

    fix(sql): Make SQL scripts compatible with MySQL and simplify

--- core/src/main/resources/database/1.sql
+++ core/src/main/resources/database/1.sql
@@ -1,29 +1,25 @@
 CREATE TABLE IF NOT EXISTS ecinv_main
 (
-    id      INT NOT NULL,
-    version INT NOT NULL,
-    CONSTRAINT ecinv_main_pk PRIMARY KEY (id)
+    id      INT NOT NULL PRIMARY KEY,
+    version INT NOT NULL
 );
 
-INSERT OR
-REPLACE
-INTO ecinv_main
+REPLACE INTO ecinv_main
 VALUES (42, 1);
 
 CREATE TABLE IF NOT EXISTS ecinv_inventories
 (
-    id      CHAR(36)     NOT NULL,
+    id      CHAR(36)     NOT NULL PRIMARY KEY,
     layout  VARCHAR(255) NOT NULL,
-    content TEXT         NOT NULL,
-    CONSTRAINT ecinv_inventories_pk PRIMARY KEY (id)
+    content TEXT         NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ecinv_bindings
 (
     holder_id    CHAR(36) NOT NULL,
     inventory_id CHAR(36) NOT NULL,
-    CONSTRAINT ecinv_inventory_bindings_pk PRIMARY KEY (holder_id, inventory_id),
-    CONSTRAINT ecinv_inventory_bindings_ecinv_inventories_id_fk FOREIGN KEY (inventory_id)
-        REFERENCES ecinv_inventories
+    PRIMARY KEY (holder_id, inventory_id),
+    FOREIGN KEY (inventory_id)
+        REFERENCES ecinv_inventories (id)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
